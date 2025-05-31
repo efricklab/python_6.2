6647b637b4743945791caefbd521ba84d47da86b
efricklab <erfrick@gmail.com>
2025-05-31 15:07:16 -0400

    Create loops_lab.py

---
+++ loops_lab.py
@@ -0,0 +1,5 @@
+# For loop example: Iterating through a list of fruits
+fruits = ["apple", "banana", "cherry"]
+print("For loop: Iterating through a list of fruits")
+for fruit in fruits:
+    print(fruit)
